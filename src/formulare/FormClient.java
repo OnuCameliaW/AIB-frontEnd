7357db985ad6f538ba612a0cef06c50bd679b9cb
Onu Camelia <onu.camelia@yahoo.com>
2018-01-05 20:59:40 +0200

    basic

---
+++ src/formulare/FormClient.java
@@ -0,0 +1,23 @@
+package formulare;
+
+import com.sample.ExamplePK;
+
+public class FormClient {
+	private ExamplePK exPK;
+
+	public ExamplePK getExPK() {
+		return exPK;
+	}
+
+	public void setExPK(ExamplePK exPK) {
+		this.exPK = exPK;
+	}
+
+	public FormClient() {
+		super();
+		// TODO Auto-generated constructor stub
+		// ce incarc din DB 
+	}
+	
+
+}
